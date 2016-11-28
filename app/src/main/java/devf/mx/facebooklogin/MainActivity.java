8fc08a331b24ad8f091ee737b4b65057446fd100
KenMarquez <darius_dam@hotmail.com>
2016-11-27 21:32:09 -0600

    Registramos el callback de Facebook

--- app/src/main/java/devf/mx/facebooklogin/MainActivity.java
+++ app/src/main/java/devf/mx/facebooklogin/MainActivity.java
@@ -1,12 +1,17 @@
 package devf.mx.facebooklogin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FacebookCallback<LoginResult> {
 
     private LoginButton loginButton;
     private CallbackManager callbackManager;
@@ -21,8 +26,52 @@ public class MainActivity extends AppCompatActivity {
         //Referenciamos el boton de facebook creado en el layout
         loginButton = (LoginButton) findViewById(R.id.login_button);
 
+        //Los permisos sirven para soliciatrle al usuario acceso a ciertos datos de su cuenta de Facebook
+        loginButton.setReadPermissions("email");
 
+        /**
+         * Le indicamos el callbackManager para
+         */
+        loginButton.registerCallback(callbackManager, this);
+    }
+
+    /**
+     * Resultado de la respuesta de Facebook
+     *
+     * @param loginResult
+     */
+    @Override
+    public void onSuccess(LoginResult loginResult) {
+        Log.i("success", "success");
+    }
+
+    /**
+     * Se ejecuta cuando se interrumpe el proceso de inicio de sesión con Facebook
+     */
+    @Override
+    public void onCancel() {
+        Log.i("error", "cancel");
+    }
+
+    /*
+     * @param error
+     */
+    @Override
+    public void onError(FacebookException error) {
+        /**
+         * Implementación de código en caso de que halla ocurrido algún error
+         */
+        Log.e("facebookError", error.getMessage());
     }
 
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        /**
+         * Redireccionamos el resultado de la respuesta de Facebook para que el
+         * CallbackManager se encargue de llamar los metodos indicados
+         */
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 }
